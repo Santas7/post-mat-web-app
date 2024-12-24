1cf9284ec89a776277632344684c60eb582b3ffe
Andrei <86359412+Santas7@users.noreply.github.com>
2024-12-24 06:09:23 -0800

    Update PinCodeDisplay.jsx

--- src/components/PinCode/PinCodeDisplay.jsx
+++ src/components/PinCode/PinCodeDisplay.jsx
@@ -1,6 +1,16 @@
 import styles from './PinCodeDisplay.module.css';
 
 export default function PinCodeDisplay({ pin }) {
-  return <input className={styles.pinCode} type="text" value={pin} readOnly />
-}
+  const pinWithPlaceholders = Array(6).fill('_');
+  for (let i = 0; i < pin.length; i++) pinWithPlaceholders[i] = pin[i];
 
+  return (
+    <input
+      className={styles.pinCode}
+      type="text"
+      value={pinWithPlaceholders.join(' ')} 
+      readOnly
+      placeholder="____ __" 
+    />
+  );
+}
